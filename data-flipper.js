9162c9fe4eef174ec33a6c0a84f8e5cae859c3a8
Jon Christensen <JCodesMN@gmail.com>
2017-06-20 15:42:27 -0500

    Tue Jun 20 15:42:29 2017

--- data-flipper.js
+++ data-flipper.js
@@ -93,6 +93,7 @@ function openFileAsSpreadsheet(file) {
 // script
 
 function runScript() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
   var iValues;
   if (config.process !== "") {
     if (config.input.pathToSpreadsheet !== "" && config.input.sheet !== "") {
@@ -104,7 +105,7 @@ function runScript() {
           if (iSheet) {
             var iMaxRows = iSheet.getMaxRows();
             var iMaxCol  = iSheet.getMaxColumns();
-            iValues = iSheet.getRange(2, 1, iMaxRows, iMaxCol).getValues();
+            iValues      = iSheet.getRange(2, 1, iMaxRows, iMaxCol).getValues();
           } else {
             // sheet not found in spreadsheet
           }
@@ -121,9 +122,9 @@ function runScript() {
     // no process specified
   }
 
+  // do stuff with iValues
 
   var oFile, oSS, oSheet;
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
 
 } 
 
@@ -138,34 +139,34 @@ function runScript() {
 // var iColCount = iSheet.getMaxColumns();
 // var aValues   = iSheet.getRange(2,1,iRowCount,iColCount).getValues();
 
-// function aToData() {
-//     var bValues = [];
-//     for (var aRow in aValues) {
-//         bValues[aRow] = [];
-// 		bValues[aRow].push(aValues[aRow][0]);
-// 		for (i = 1; i < iColCount; i++) {
-// 			if (aValues[aRow][i] !== "") {
-// 			    var class1  = aValues[aRow][i];
-//                 bValues[aRow].push(class1);
-// 	        } else {
-//                 break;
-// 		    }
-//         }
-// 	}
-//     var cValues = [];
-// 	var cRowCount = 0;
-// 	for (var bRow in bValues) {
-// 	    for (i = 1; i < bValues[bRow].length; i++) {
-// 		    var student        = bValues[bRow][0];
-// 			var class2          = bValues[bRow][i];
-// 			cValues[cRowCount] = [];
-// 			cValues[cRowCount].push(student,class2);
-// 			cRowCount++;
-//         }
-// 	}
-//   var oRowCount = cValues.length; 
-//   ss.setActiveSheet(oSheet);
-//   var oRange = oSheet.getRange(2,1,oRowCount,2);
-//   oRange.setValues(cValues);
-// }
+function aToData() {
+    var bValues = [];
+    for (var aRow in aValues) {
+        bValues[aRow] = [];
+		bValues[aRow].push(aValues[aRow][0]);
+		for (i = 1; i < iColCount; i++) {
+			if (aValues[aRow][i] !== "") {
+			    var class1  = aValues[aRow][i];
+                bValues[aRow].push(class1);
+	        } else {
+                break;
+		    }
+        }
+	}
+    var cValues = [];
+	var cRowCount = 0;
+	for (var bRow in bValues) {
+	    for (i = 1; i < bValues[bRow].length; i++) {
+		    var student        = bValues[bRow][0];
+			var class2          = bValues[bRow][i];
+			cValues[cRowCount] = [];
+			cValues[cRowCount].push(student,class2);
+			cRowCount++;
+        }
+	}
+  var oRowCount = cValues.length; 
+  ss.setActiveSheet(oSheet);
+  var oRange = oSheet.getRange(2,1,oRowCount,2);
+  oRange.setValues(cValues);
+}
 
